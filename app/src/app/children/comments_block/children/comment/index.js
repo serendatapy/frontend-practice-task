8e3d16e5a443c8a65183417913602a3e7f5079f1
serendatapy <arvaldivia@gmail.com>
2021-11-20 21:23:32 +0000

    feat: added hyperlink and hashtag anchor tags

--- app/src/app/children/comments_block/children/comment/index.js
+++ app/src/app/children/comments_block/children/comment/index.js
@@ -2,12 +2,17 @@ import React from 'react'
 import styles from './styles'
 
 const Comment = (props) => {
+  const formatAts = text => {
+    const regex = /(@[a-z0-9\_.]*|#[a-z0-9]*)/gi;
+    return {__html: text.replace(regex, '<a href="#" style="color: blue">$1</a>')};
+  }
+
   const {username, userComment} = props
   return (
     <div>
       <p>
         <span style={styles.username}>{username}</span>
-        <span style={styles.userComment}>{userComment}</span>
+        <span style={styles.userComment} dangerouslySetInnerHTML={formatAts(userComment)}></span>
       </p>
     </div>
   )
